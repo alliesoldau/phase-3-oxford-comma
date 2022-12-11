94e77d862a5c35dbd73d43c9eb7010af8f2e60e4
Allie Soldau <alliesoldau@gmail.com>
2022-12-11 07:12:22 -0500

    commit tree 8fa185bad4b5525c13006a727080b2582602f92a

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,27 @@
-def oxford_comma(array)
+# array = ["fiddleheads", "okra", "kohlrabi"]
+# array = ["okra"]
+# array = ["fiddleheads", "okra"]
 
+def oxford_comma(array)
+    if array[0] == array.last
+        array[0]
+    elsif array.length == 2
+        array.join(" and ")
+    else
+        reduce = array.filter do |item|
+            # puts item
+            unless item == array.last
+                item
+            end
+        end
+        joined = reduce.join(", ")
+        # puts joined
+        lastItem = ", and #{array.last}"
+        # puts lastItem
+        final = joined << lastItem
+        final
+    end
 end
+
+# oxford_comma(array)
+
